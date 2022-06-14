9e48a9d83d536e3886d953c03f4336d6c8d18eef
Masih-Saldanha <masih.saldanha@gmail.com>
2022-06-13 22:33:56 -0300

    feat: add tables.sql content

---
+++ tables.sql
@@ -0,0 +1,32 @@
+CREATE TABLE "users" (
+	"id" serial NOT NULL PRIMARY KEY,
+	"email" text NOT NULL UNIQUE,
+	"password" varchar(8) NOT NULL,
+	"username" varchar(100) NOT NULL,
+	"pictureUrl" text NOT NULL
+);
+
+CREATE TABLE "sessions" (
+	"id" serial NOT NULL PRIMARY KEY,
+	"token" TEXT NOT NULL UNIQUE,
+	"userId" integer NOT NULL REFERENCES "users"("id")
+);
+
+CREATE TABLE "posts" (
+	"id" serial NOT NULL PRIMARY KEY,
+	"description" varchar(140),
+	"link" TEXT NOT NULL,
+	"userId" integer NOT NULL REFERENCES "users"("id")
+);
+
+CREATE TABLE "likes" (
+	"id" serial NOT NULL PRIMARY KEY,
+	"userId" integer NOT NULL REFERENCES "users"("id"),
+	"postId" integer NOT NULL REFERENCES "posts"("id")
+);
+
+CREATE TABLE "hashtags" (
+	"id" serial NOT NULL PRIMARY KEY,
+	"hashtag" varchar(140) NOT NULL,
+	"postId" integer NOT NULL REFERENCES "posts"("id")
+);
