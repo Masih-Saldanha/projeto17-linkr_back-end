a0716e1be15af938dbb47f3711c9e6884e9fb037
simone-alcala <simone.alcala@gmail.com>
2022-06-22 23:05:57 -0300

    FIX: added auth to comments route

--- src/routers/commentsRouter.js
+++ src/routers/commentsRouter.js
@@ -2,12 +2,13 @@
 import { Router } from 'express';
 
 import { getCommentsByPostId, addComment } from '../controllers/commentsController.js';
+import { validaHeader } from '../middlewares/authMiddleware.js';
 import { validateSchema } from '../middlewares/validateSchema.js';
 import commentsSchema from '../schemas/commentsSchema.js';
 
 const commentsRouter = Router();
 
-commentsRouter.get('/comment/:postid', validateSchema(commentsSchema.getCommentSchema), getCommentsByPostId);
-commentsRouter.post('/comment', validateSchema(commentsSchema.addCommentSchema), addComment);
+commentsRouter.get('/comment/:postid', validaHeader, validateSchema(commentsSchema.getCommentSchema), getCommentsByPostId);
+commentsRouter.post('/comment', validaHeader, validateSchema(commentsSchema.addCommentSchema), addComment);
 
 export default commentsRouter;
